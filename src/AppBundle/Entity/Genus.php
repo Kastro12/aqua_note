41d315728cd2d1528860d47bba0be4c4f045ec7c
Aleksandar <aleksandarrlm@gmail.com>
2017-11-27 18:51:22 +0100

    Created aqua_note database and genus table

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sale
+ * Date: 11/27/2017
+ * Time: 6:39 PM
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+}
